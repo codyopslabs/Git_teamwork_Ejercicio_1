4d99f521f880c35cce0d415e27b7ee60e832f328
CodyOps <codyopslabs@gmail.com>
2023-12-20 10:13:15 -0300

    Update homework.js
    Hola profesor, adjunto el ejercicio completado!
    - María Gutierrez

--- homework.js
+++ homework.js
@@ -1,1 +1,16 @@
 //Corrija la tarea de su alumno
+
+// Definir la función para calcular el área de un círculo
+function calcularAreaCirculo(radio) {
+  // Usar la fórmula A = π * r^2 para calcular el área
+  var area = Math.PI * Math.pow(radio, 3);
+  return area;
+}
+
+// Ejemplo de uso de la función
+var radioCirculo = 5;
+var areaDelCirculo = calcularAreaCirculo(radioCirculo);
+
+// Mostrar el resultado en la consola
+console.log("El área del círculo con radio " + radioCirculo + " es: " + areaDelCirculo);
+
